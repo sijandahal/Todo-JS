f2824dbf820746c125c108c736694051fd740688
Sijan Dahal <sijan.kanchan@gmail.com>
2022-07-31 10:13:10 +0545

    added the functionality and gitignore updated

--- script.js
+++ script.js
@@ -1,35 +1,59 @@
 window.addEventListener("load", () => {
-    const todoListLength = document.querySelectorAll(".todo--items li").length;
-    todoLength.innerHTML = todoListLength;
-})
+  countElement();
+  deleteElement();
+});
 
 let form = document.querySelector(".form-todo");
 let inputValue = document.querySelector(".todoinput");
-const todoItems = document.querySelector(".todo--items");
-const todoLength = document.querySelector(".todo-length span");
+const todoItemsWrapper = document.querySelector(".todo--items--wrap");
+let todoLength = document.querySelector(".todo-length span");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
+  addTodos();
+  deleteElement();
+});
+
+function addTodos() {
   let todoListItemsValue = inputValue.value;
   if (todoListItemsValue == "") {
     alert("Please enter something");
     return;
   }
-  console.log(todoListItemsValue);
-
-  todoItems.innerHTML += `
-    <li> 
-      <input type = "checkbox" id = "${todoListItemsValue}" class = "custom-checkbox"/>
-      <label for = "${todoListItemsValue}">
-      ${todoListItemsValue} 
-      </label>
-     
-     </li>
-  `;
   inputValue.value = "";
+  todoItemsWrapper.innerHTML += `
+  <div
+  class="todo--list w-full px-6 py-5 rounded-md bg-dark-desaturated-blue flex items-center justify-between"
+>
+  <div class="flex items-center justify-center">
+    <span
+      class="check w-6 h-6 items-center justify-center inline-flex rounded-full mr-5 bg-black"
+    >
+      <img src="./images/icon-check.svg" alt="Checkmark" />
+    </span>
+    <p>${todoListItemsValue}</p>
+  </div>
 
-  const todoListLength = document.querySelectorAll(".todo--items li").length;
-  todoLength.innerHTML = todoListLength;
+  <button class="cross">
+    <img src="./images/icon-cross.svg" alt="" />
+  </button>
+</div>
+  `;
+  countElement();
+}
+//addtodos
 
-  
-});
+function deleteElement() {
+  const deleteButtons = document.querySelectorAll(".cross");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const parentDiv = button.parentNode;
+      parentDiv.remove();
+      countElement();
+    });
+  });
+}
+
+function countElement() {
+  todoLength.innerHTML = document.querySelectorAll(".todo--list").length;
+}
